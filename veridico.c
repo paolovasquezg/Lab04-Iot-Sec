d437e9bcb070eec003e24cac7d860abf2862c9f2
Paolo Vásquez <83627663+paolovasquezg@users.noreply.github.com>
2025-11-11 11:53:45 -0500

    Add files via upload

---
+++ veridico.c
@@ -0,0 +1,126 @@
+
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <DHT.h>
+#include <LiquidCrystal_I2C.h>
+
+int lcdColumns = 16;
+int lcdRows = 2;
+LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);
+
+#define DHTPIN 15
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
+
+const char* ssid = "Wokwi-GUEST";
+const char* password = "";
+const char* mqtt_server = "broker.hivemq.com";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+unsigned long lastMsg = 0;
+float temp = 0;
+float hum = 0;
+
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println();
+  Serial.println("WiFi conectado");
+  Serial.print("IP: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Mensaje MQTT [");
+  Serial.print(topic);
+  Serial.print("]: ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Intentando conexión MQTT...");
+    String clientId = "ESP32-Client-";
+    clientId += String(random(0xffff), HEX);
+
+    if (client.connect(clientId.c_str())) {
+      Serial.println("Conectado al broker");
+      client.publish("/ThinkIOT/Publish", "ESP32 conectado");
+      client.subscribe("/ThinkIOT/Subscribe");
+    } else {
+      Serial.print("Error, rc=");
+      Serial.print(client.state());
+      Serial.println(" Reintentando en 5s");
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Iniciando...");
+
+  dht.begin();
+
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void loop() {
+  if (!client.connected()) reconnect();
+  client.loop();
+
+  unsigned long now = millis();
+  if (now - lastMsg > 2000) { 
+    lastMsg = now;
+
+    hum = dht.readHumidity();
+    temp = dht.readTemperature();
+
+    if (isnan(hum) || isnan(temp)) {
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("Error sensor!");
+      Serial.println("Falla en sensor DHT");
+      return;
+    }
+
+    client.publish("/ThinkIOT/temp", String(temp, 2).c_str());
+    client.publish("/ThinkIOT/hum", String(hum, 2).c_str());
+
+    Serial.print("Temp: "); Serial.print(temp); Serial.print(" °C   ");
+    Serial.print("Hum: "); Serial.print(hum); Serial.println(" %");
+
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("T:");
+    lcd.print(temp);
+    lcd.print("C");
+
+    lcd.setCursor(0, 1);
+    lcd.print("H:");
+    lcd.print(hum);
+    lcd.print("%");
+  }
+}
